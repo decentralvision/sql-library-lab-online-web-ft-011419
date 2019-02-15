a5cd3672eee55ed38909d4ff52ca44466f29cd4b
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-15 17:19:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,11 +3,11 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1"
+  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1"
+  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
@@ -15,7 +15,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM series JOIN characters ON series.id = series_id GROUP BY title ORDER BY COUNT(species = 'human') DESC LIMIT 1"
+  "SELECT title FROM series JOIN characters ON series.id = series_id GROUP BY title ORDER BY COUNT(species = 'human') DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
